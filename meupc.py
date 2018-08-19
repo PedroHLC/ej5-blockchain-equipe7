13f804373177e49f21fa1b8046e647babc824284
PedroHLC <root@pedrohlc.com>
2018-08-19 14:31:43 -0300

    Add set_email

--- meupc.py
+++ meupc.py
@@ -14,7 +14,7 @@ CORS(app)
 
 chain = 'http://miner.localhost:5000'
 
-@app.route('/secret/create/<string:label>/<string:priv_key>', methods=['GET'])
+@app.route('/secret/<string:label>/<string:priv_key>', methods=['GET'])
 def secret_create(label, priv_key):
     private_key = RSA.importKey(binascii.unhexlify(priv_key))
     signer = PKCS1_v1_5.new(private_key)
@@ -24,11 +24,28 @@ def secret_create(label, priv_key):
 
 @app.route('/perm/<int:perm>/<string:domain>/<string:uuid>/<string:priv_key>', methods=['GET'])
 def permission_create(perm, domain, uuid, priv_key):
-    perm = ('allow' if perm else 'deny')
+    perm = ('allow' if perm >= 1 else 'deny')
     transaction = Transaction(uuid, priv_key, perm, domain, '')
     signature = transaction.sign_transaction()
     r = requests.get(url=chain+'/transactions/new', params={'uuid':uuid,'type':perm, 'label':domain, 'value':'','signature':signature})
-    return r.text
+    return "", r.status_code
+
+@app.route('/email/<string:email>/<string:uuid>/<string:priv_key>', methods=['GET'])
+def set_email(email, uuid, priv_key):
+    label = 'email.primary'
+    private_key = RSA.importKey(binascii.unhexlify(priv_key))
+    signer = PKCS1_v1_5.new(private_key)
+    h = SHA.new(label.encode('utf-8'))
+    secret_key = signer.sign(h)
+
+    value = '{"email":"'+email+'"}'
+    encrypted = val_encrypt(value, label, priv_key)
+    type_ = 'sign'
+    
+    transaction = Transaction(uuid, priv_key, type_, label, encrypted)
+    signature = transaction.sign_transaction()
+    r = requests.get(url=chain+'/transactions/new', params={'uuid':uuid,'type':type_, 'label':label, 'value':encrypted,'signature':signature})
+    return "", r.status_code
 
 if __name__ == '__main__':
     from argparse import ArgumentParser
